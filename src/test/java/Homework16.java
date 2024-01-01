d6ab4f9dace9e7b85fa702b4aa1e182ef9924492
cll2 <75150662+cll2@users.noreply.github.com>
2023-12-31 21:01:23 -0500

    registration test
    created registrationNavigation test

---
+++ src/test/java/Homework16.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Homework16 extends BaseTest{
+
+    @Test
+    public void RegistrationNavigation() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://qa.koel.app/";
+        driver.get(url);
+
+        WebElement registrationButton = driver.findElement(By.cssSelector("[href='registration']"));
+
+        registrationButton.click();
+
+        Assert.assertEquals(driver.getCurrentUrl(), "https://qa.koel.app/registration");
+
+        driver.quit();
+
+
+    }
+}
